79c328e75b5df4b6247384ff8bdbb4bdc470760c
Salim <salimbabaji@gmail.com>
2023-01-15 10:15:54 +0100

    Can use diff size of squares

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const container = document.querySelector("#container");
 const squares = document.createElement('div');
 
-squares.setAttribute('style', 'width: 16px; height: 16px; border-style: solid; border-color: black;');
+squares.setAttribute('style', 'width: 16px; height: 16px; border-style: solid; border-color: black; padding: 0px; box-sizing: border-box; border-width: 0.2px');
 
 container.appendChild(squares);
 
-for (i =0; i<1673; i++){
+const containerHeight = container.clientHeight;
+const containerWidth = container.clientWidth;
+
+
+let noOfSquares = (size) => {
+    let height = containerHeight /size;
+    let width = containerWidth /size;
+ 
+    let squares = width * height;
+
+
+   
+    return squares;
+
+}
+
+console.log(noOfSquares(16));
+
+
+
+
+
+for (i =0; i<noOfSquares(16); i++){
     container.appendChild(squares.cloneNode());
 }
 
-console.log(window.innerHeight);
-console.log(window.innerWidth)
+console.log(containerHeight);
+console.log(containerWidth)
+
