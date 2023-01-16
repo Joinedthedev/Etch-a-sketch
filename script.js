6eceeb648abf01c1a08c01a4fbc0d11a27af8919
Salim <salimbabaji@gmail.com>
2023-01-16 09:41:07 +0100

    Added a clear button that clears the board

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const container = document.querySelector("#container");
-let square = document.querySelector('#squares');
+const container = document.querySelector("#container");// reference for container
+let square = document.createElement('div'); //creating sqaure to be cloned
+
+const clearButton= document.querySelector("#clear");
 
 square.setAttribute('style', 'width: 16px; height: 16px; border-style: solid; border-color: black; padding: 0px; box-sizing: border-box; border-width: 0.2px');
 
@@ -21,22 +23,26 @@ let noOfSquares = (size) => {
 
 }
 
-console.log(noOfSquares(16));
 
 
 
+/*This is what creates a grid of squares by cloning 1 square a set number of times based on the noOfSquares function.
+By pushing each sqaure into an array, we get a reference to it via its index allowing it to be interacted with using
+events. Hence, the use of the foreach.  ETC*/
 let squareArray = [];
-
+squareArray.push(square);
 for (i =0; i<noOfSquares(16); i++){
     let squareClone = square.cloneNode();
     container.appendChild(squareClone);
     squareArray.push(squareClone);
 }
 
-const squares = document.querySelectorAll('#squares');
+const clearBoard = ()=>{
+  squareArray.forEach((square)=> square.style.backgroundColor = "white");
+}
 
-console.log(containerHeight);
-console.log(containerWidth)
-console.log(square);
+//Event Listeners
 squareArray.forEach( (square)=>{
-    square.addEventListener('mouseover', ()=> square.style.backgroundColor = "red")}) ;
+    square.addEventListener('mouseover', ()=> square.style.backgroundColor = "black")}) ;
+
+clearButton.addEventListener('click', ()=>clearBoard());
